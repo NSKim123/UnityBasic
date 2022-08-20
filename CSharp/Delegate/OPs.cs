359310f88ee5a1785371b523d3f9d41329c1940e
gwanwook <elsgundam@naver.com>
2022-08-20 15:54:39 +0900

    week5
    delegate(event, Action, Func, Anonymous, Lambda
    enumeraotor(usage of IEnumerable, IEnumerator, Traveler)

---
+++ CSharp/Delegate/OPs.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    public class OPs
+    {
+        public static int opCount;
+        public static void Init()
+        {
+            Program.RegisterCallBack(Sum);
+            Program.RegisterCallBack(Sub);
+            Program.opDelegate += Mul;
+            // Program.opDelegate(1,2); // event 때문에 에러가 뜸.
+        }
+        public static int Sum(int a, int b) //int 형태로 정의했으면 int로 반환해야하는 것이 하나 있어야 한다. 없다면 void 사용
+        {
+            Console.WriteLine($"OP : Sum() , result : {a+b}");
+         opCount++;     
+            return a + b;   //int 형태로 정의했으면 int로 반환해야하는 것이 하나 있어야 한다.
+        }
+
+        public static int Sub(int a, int b)
+        {
+            Console.WriteLine($"OP : Sub() , result : {a - b}");
+            opCount++;
+            return a - b;
+        }
+
+        public static int Mul(int a, int b)
+        {
+            Console.WriteLine($"OP : Mul() , result : {a * b}");
+            opCount++;
+            return a * b;
+        }
+
+        public static int Div(int a, int b)
+        {
+            Console.WriteLine($"OP : Div() , result : {a / b}");
+            opCount++;
+            return a / b;
+        }
+
+        public static int Mod(int a, int b)
+        {
+            Console.WriteLine($"OP : Mod() , result : {a % b}");
+            opCount++;
+            return a % b;
+        }
+
+        
+    }
+}
---
+++ CSharp/Delegate/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Delegate;
+namespace Delegate
+{
+    internal class Program
+    {
+        public delegate int DelegateHandler(int a, int b);
+        public static event DelegateHandler opDelegate;
+
+        //event 한정자
+        //delegate를 위한 한정자
+        //외부 클래스에서 함수를 대리자에 등록하거나 제거할수는 있으나 직접 대리자를 호출 할 수는 없도록 하는 한정자
+
+        public static Action<int, int> opAction;
+        public static Func<int, int, int> opFunc;
+
+        static void Main(string[] args)
+        {
+            //Delegate : 대리자
+            //함수 포인터 타입 ( 함수를 대입해뒀다가 호출할 수 있도록 해줌 )
+            // delegate 키워드로 대리자를 정의할 수 있음
+            //정의한 대리자 타입의 대리자변수를 선언해서 사용함.
+
+            //함수체이닝
+            //Delegate += OPs.Sum;
+            //Delegate += OPs.Sub; //opDelegate라는 함수에 Sum Sub 함수가 순서대로 추가됨.
+            //Delegate += OPs.Mul;
+            //Delegate += OPs.Div;
+            //Delegate += OPs.Mod;
+            //
+            //Delegate -= OPs.Mod;
+
+            OPs.Init();
+
+            while (true)
+            {
+                Console.WriteLine("연산을 수행하고 싶으면 DoOP를 입력하세요.");
+                string input = Console.ReadLine();
+                if (input == "DoOP")
+                {
+                    int a = 0, b = 0;
+                    Console.WriteLine("첫번째 피연산자를 입력하세요");
+                    try
+                    {
+                        a = Int32.Parse(Console.ReadLine());
+                    }
+                    catch (FormatException e)
+                    {
+                        Console.WriteLine("아니 정수 입력 하시라고요.");
+                        Console.WriteLine(e.Message);
+                        continue; //다시 상위 반복문으로 돌아가는 것.
+                    }
+                    finally
+                    {
+                        // 예외 catch가 되든 안되는 마지막에 수행하고 싶은 내용
+                    }
+                    a = Int32.Parse(Console.ReadLine());
+                    Console.WriteLine("두번째 피연산자를 입력하세요");
+                    try
+                    {
+                        b = Int32.Parse(Console.ReadLine());
+                    }
+                    catch
+                    {
+                        Console.WriteLine("아니 정수 입력 하시라고요.");
+                    }
+                    b = Int32.Parse(Console.ReadLine());
+
+                    //OPs.Sum(a, b);
+                    //OPs.Sub(a, b);
+                    //OPs.Mul(a, b);
+                    //OPs.Div(a, b);
+                    //OPs.Mod(a, b);
+                    opDelegate(a, b);
+                    //익명 메소드 등록
+                    opAction += delegate (int a, int b)
+                    {
+                        Console.WriteLine(a + b);
+                    };
+                    opAction(1, 2);
+
+                    //람다식 등록( => 를 사용하면 람다식이라고 함.)
+                    opAction += (a, b) => { Console.WriteLine(a + b); };
+
+                }
+
+            }
+        }
+
+        public static void RegisterCallBack(DelegateHandler delegateHandler)
+        {
+            opDelegate += delegateHandler;
+        }
+    }
+}
