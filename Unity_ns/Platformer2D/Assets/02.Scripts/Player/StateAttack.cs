108cae819213845fcee33a5dcd31eaf21d3f46bd
gwanwook <elsgundam@naver.com>
2022-10-01 18:50:36 +0900

    Platformer2D
    Scripts-
    IState(interface)
    StateBase(abstract) : State(Idle,Jump,Fall)
    StateMachine(constructorInfo, Reflextion)
    GroundDetector

---
+++ Unity_ns/Platformer2D/Assets/02.Scripts/Player/StateAttack.cs
@@ -0,0 +1,35 @@
+﻿public class StateAttack : StateBase
+{
+    private GroundDetector _groundDetector;
+    public StateAttack(StateMachine.StateType machineType, StateMachine machine) : base(machineType, machine)
+    {
+        _groundDetector = machine.GetComponent<GroundDetector>();
+    }
+
+    public override bool IsExecuteOK { get; }
+
+    public override void Execute()
+    {
+        
+    }
+
+    public override void FixedUpdate()
+    {
+        
+    }
+
+    public override void ForceStop()
+    {
+        
+    }
+
+    public override void MoveNext()
+    {
+        
+    }
+
+    public override StateMachine.StateType Update()
+    {
+        return MachineType;
+    }
+}
